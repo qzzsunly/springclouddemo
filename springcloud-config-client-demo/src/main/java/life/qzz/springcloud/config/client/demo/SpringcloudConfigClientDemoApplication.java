ef4add93020a1c429b487b167af2a39cc6984c04
qzz <qzzsunly@163.com>
2017-12-13 17:08:13 +0800

    config demo

--- springcloud-config-client-demo/src/main/java/life/qzz/springcloud/config/client/demo/SpringcloudConfigClientDemoApplication.java
+++ springcloud-config-client-demo/src/main/java/life/qzz/springcloud/config/client/demo/SpringcloudConfigClientDemoApplication.java
@@ -3,11 +3,13 @@ package life.qzz.springcloud.config.client.demo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@RefreshScope
 public class SpringcloudConfigClientDemoApplication {
 
 	public static void main(String[] args) {
