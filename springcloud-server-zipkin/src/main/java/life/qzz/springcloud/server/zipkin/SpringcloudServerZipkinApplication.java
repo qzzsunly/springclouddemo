ccbbcfc6e12bc900c741af64e3f93db6827a85ef
qzz <qzzsunly@163.com>
2017-12-14 20:45:03 +0800

    zipkin server demo

---
+++ springcloud-server-zipkin/src/main/java/life/qzz/springcloud/server/zipkin/SpringcloudServerZipkinApplication.java
@@ -0,0 +1,14 @@
+package life.qzz.springcloud.server.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin.server.EnableZipkinServer;
+
+@EnableZipkinServer
+@SpringBootApplication
+public class SpringcloudServerZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudServerZipkinApplication.class, args);
+	}
+}
